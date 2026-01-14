560b62926cf6c94210a5815a0e077b1ec8ac03dd
developerTamanna <astamanna57@gmail.com>
2026-01-14 13:45:13 +0600

    favicon.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,17 @@ import './globals.css';
 export const metadata = {
   title: 'Tamanna Akter - Frontend & Backend Developer',
   description: 'Portfolio built with Next.js and Tailwind',
+  icons: {
+    icon: '/public/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className="font-sans">
         <Header />
         <div className="min-h-screen mt-16">{children}</div>
